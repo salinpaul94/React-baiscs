04b0dd42503bccef793b0cbf58ef24881103dfb2
salinpaul94 <salinpaul@hotmail.com>
2022-11-09 03:01:16 +0530

    setState prevState

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
 import Message from './components/Message';
-
+import Counter from './components/Counter';
 function App() {
   return (
     <div className="App">
-      <Message/>
+      <Counter/>
+      {/* <Message/> */}
       {/* <Greet name="Salinpaul" lastname="Valooran">
         <button>hello</button>
       </Greet>
