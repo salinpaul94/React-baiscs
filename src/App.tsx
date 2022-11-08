7bc254a2ed3c05bba514bceb2702f7818b2af036
salinpaul94 <salinpaul@hotmail.com>
2022-11-09 02:42:58 +0530

    class state

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,19 @@ import React, {Component} from 'react';
 import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
+import Message from './components/Message';
 
 function App() {
   return (
     <div className="App">
-      <Greet name="Salinpaul" lastname="Valooran">
+      <Message/>
+      {/* <Greet name="Salinpaul" lastname="Valooran">
         <button>hello</button>
       </Greet>
       <Greet name="Bruce" lastname="Vane">
         <p>This is children props</p>
       </Greet>
-      <Welcome/>
+      <Welcome/> */}
     </div>
   );
 }
