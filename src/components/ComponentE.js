ff48fd812088efd32ad83cebecb2a2bc15dc082e
salinpaul94 <salinpv@hotmail.com>
2022-11-14 03:15:12 +0530

    context default value and context type

--- src/components/ComponentE.js
+++ src/components/ComponentE.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react'
 import ComponentF from './ComponentF'
+import UserContext from './UserContext'
 
 class ComponentE extends Component {
+    static contextType = UserContext  // it only works with class component, and only one value can be consumed
   render() {
     return (
-      <div><ComponentF/></div>
+    //   <div><ComponentF/></div>
+    <div>
+        Component E context {this.context}
+        <ComponentF/>
+    </div>
     )
   }
 }
+
+// ComponentE.contextType = UserContext
 
 export default ComponentE
--- src/components/UserContext.js
+++ src/components/UserContext.js
@@ -1,8 +1,10 @@
 import React from "react";
 
-const UserContext = React.createContext()
+
+const UserContext = React.createContext('CodeEvolution') //default value
 
 const UserProvider = UserContext.Provider
 const UserConsumer = UserContext.Consumer
 
 export { UserConsumer, UserProvider}
+export default UserContext
