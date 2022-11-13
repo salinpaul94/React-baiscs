4d4031a802a5d810e30ec90eabf990b3f34027e6
salinpaul94 <salinpv@hotmail.com>
2022-11-13 23:45:02 +0530

    react Fragments

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-
+import FragmentDemo from './components/FragmentDemo';
+import FragmentDemo2 from './components/FragmentDemo2';
+import Table from './components/Table';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <br/>
+
+      <Table/>
+      <FragmentDemo2/>
+      <FragmentDemo/>
+      <br/><br/>
+      <div>
+        Testing
+      </div>
     </div>
   );
 }
---
+++ src/components/FragmentDemo.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function FragmentDemo() {
+  return (
+    <>
+        <h1>
+            FragmentDemo
+        </h1>
+        <p>This is the discription of fragment</p>
+    </>
+  )
+}
+
+export default FragmentDemo
